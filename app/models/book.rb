0089ce0d2c671f81e219234b944d65aeaa53b0e6
Ubuntu <ubuntu@ip-172-31-20-1.ap-northeast-1.compute.internal>
2025-10-07 08:52:16 +0000

    [Add]投稿一覧ページ

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,13 @@
 class Book < ApplicationRecord
     has_one_attached :image
     belongs_to :user
+
+    # 画像一覧にユーザー画像を入れたい
+    def get_image
+        if image.attached?
+            image
+        else
+            'no_image.jpg'
+        end
+    end
 end
