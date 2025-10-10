9a7b25586a0d3b57724fe16b69cbc73017d08db5
Ubuntu <ubuntu@ip-172-31-20-1.ap-northeast-1.compute.internal>
2025-10-10 02:55:23 +0000

    [Add]userのeditページ

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   get "/homes/about" => "homes#about", as: "about"
   devise_for :users
 
-  resources :books, only: [:new, :create, :index, :show]
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :books, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only:[:show, :edit]
 end
